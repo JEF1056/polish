21e20633e364c4e20d95617932e10bd7eb9fa5c1
Jess Fan <jessefan2002@gmail.com>
2022-10-15 16:26:15 -0700

    cool todo moment

---
+++ ML/train.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from simplet5 import SimpleT5
+from sklearn.model_selection import train_test_split
+
+# Load the csv as a pandas dataframe
+train_ds = pd.read_csv("asset-valid.csv").dropna().astype(str)
+valid_ds = pd.read_csv("asset-test.csv").dropna().astype(str)
+
+# train_df, eval_df = train_test_split(df, test_size=0.2)
+# train_df.shape, eval_df.shape
+
+model = SimpleT5()
+model.from_pretrained("t5","t5-small")
+
+model.train(train_df=train_ds,
+            eval_df=valid_ds,
+            source_max_token_len = 64, 
+            target_max_token_len = 64,
+            batch_size = 68,
+            max_epochs = 25,
+            use_gpu = True,
+            outputdir = "asset-test-e",
+            early_stopping_patience_epochs = 2,
+            precision = 16
+            )
---
+++ TODO.md
@@ -0,0 +1,4 @@
+web:
+- Create a flask server
+- serve index.html, containing a custom "text box" (probably more like <p></p> text with js to remove/add text)
+- use jsdiff http://incaseofstairs.com/jsdiff/ https://github.com/kpdecker/jsdiff we want a word-level implementation
