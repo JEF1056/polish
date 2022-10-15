d49b57642396bd03301e574eaecabc2cc10f9d6d
lineUCB <106828511+lineUCB@users.noreply.github.com>
2022-10-15 16:10:41 -0700

    Update main.py
    Flask starter

--- web/main.py
+++ web/main.py
@@ -2,4 +2,7 @@ from flask import Flask
 # pip install flask
 app = Flask(__name__)
 @app.route("/")
-@app.route("/test")
+
+def home():
+   return "This is the home page"if __name__ == "__main__":
+   app.run()
