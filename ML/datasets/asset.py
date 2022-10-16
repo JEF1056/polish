b735f0d9151e3835b9613c8dbbd9cf34e3be27c9
Jess Fan <jessefan2002@gmail.com>
2022-10-15 17:09:14 -0700

    asset creation

---
+++ ML/datasets/asset.py
@@ -0,0 +1,65 @@
+import os
+import copy
+import json
+import pandas as pd
+from transformers import AutoTokenizer
+
+root = "dataset"
+
+tokenizer = AutoTokenizer.from_pretrained("t5-small")
+
+splits = {
+    "valid": [],
+    "test": []
+}
+
+lengths = {
+    "valid": {
+        "source": [],
+        "target": [],
+    },
+    "test": {
+        "source": [],
+        "target": [],
+    }
+}
+
+splits_orig = {
+    "valid": open(os.path.join(root, "asset.valid.orig"), "r").read().splitlines(),
+    "test": open(os.path.join(root, "asset.test.orig"), "r").read().splitlines()
+}
+
+def cleanup(text):
+    while "  " in text:
+        text = text.replace("  ", "")
+        
+    return text.strip()
+
+for file in os.listdir("dataset"):
+    if not file.endswith(".orig"):
+        with open(os.path.join(root, file), "r") as f:
+            split = file.split(".")[1]
+            data = f.read().splitlines()
+            for i in range(len(splits_orig[split])):
+                source = cleanup(splits_orig[split][i])
+                target = cleanup(data[i])
+                est_min = len(source) - len(target)
+                # if est_min < 0: continue
+                source = "summarize: " + source
+                splits[split].append([source, target])
+                lengths[split]['source'].append(len(tokenizer(source, return_tensors="pt").input_ids[0]))
+                lengths[split]['target'].append(len(tokenizer(target, return_tensors="pt").input_ids[0]))
+
+for split in splits:
+    for group in lengths[split]:
+        lenlist = copy.deepcopy(lengths[split][group])
+        lengths[split][group] = {
+            "min":min(lenlist),
+            "max":max(lenlist),
+            "avg":sum(lenlist)/len(lenlist),
+            "std_dev": (sum([((x - sum(lenlist)/len(lenlist)) ** 2) for x in lenlist]) / len(lenlist)) ** 0.5
+        }
+    
+    pd.DataFrame(splits[split], columns = ["source_text", "target_text"]).to_csv("asset-"+split+".csv", index=False)
+    
+print(json.dumps(lengths, indent=2))
